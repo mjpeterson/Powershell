2a1e1fd16ee691537a234161b6073d84c1aa37bd
mjpeterson <matthew.john.peterson@gmail.com>
2017-02-05 13:34:55 -0600

    Add files via upload
    I use the google define:word a lot, so I thought this might be sort of neat to put into a command line script.

---
+++ Get-Define.ps1
@@ -0,0 +1,24 @@
+﻿Param (
+[Parameter(Mandatory=$True,ValueFromPipeline=$True,ValueFromPipelineByPropertyName=$True)]
+$Word
+)
+
+$url = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/$($word)"
+$headers = @{}
+$headers.Add("app_id", "<your app id>")
+$headers.Add("app_key","<your app key>")
+
+try
+{
+    $response = Invoke-WebRequest -Uri $url -Headers $headers
+    $json = ConvertFrom-Json $response.Content
+    if($json -ne $Null)
+    {
+        $json.results.lexicalEntries.entries.senses.definitions
+    }
+}
+catch
+{
+    Write-Host -ForegroundColor White -BackgroundColor Red "Word $($Word) not found"
+}
+
